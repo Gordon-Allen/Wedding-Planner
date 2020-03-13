1ef780e3c5ec89231196ebe9f6d494fab9cac6c1
Gordon-Allen <gordonmichaelallen@gmail.com>
2020-03-13 11:46:44 -0400

    Updated 'Wedding' Model's properties & types

--- Models/WeddingModel.cs
+++ Models/WeddingModel.cs
@@ -12,13 +12,41 @@ namespace WeddingPlanner.Models
         [Key]
         public int WeddingId { get; set; }
         [Required]
-        public string WedderOne { get; set; }
+        [Display(Name = "Wedder One's First Name")]
+
+        public string WedderOneFirstName { get; set; }
         [Required]
-        public string WedderTwo { get; set; }
+        [Display(Name = "Wedder One's Last Name")]
+
+        public string WedderOneLastName { get; set; }
+
         [Required]
+        [Display(Name = "Wedder Two's First Name")]
+
+        public string WedderTwoFirstName { get; set; }
+        [Required]
+        [Display(Name = "Wedder Two's Last Name")]
+
+        public string WedderTwoLastName { get; set; }
+        [Required]
+        [Display(Name = "Date & Time Of Wedding")]
         public DateTime WeddingDate { get; set; }
         [Required]
-        public string WeddingAddress { get; set; }
+        [Display(Name = "Street Address")]
+        public string WeddingStreetAddress { get; set; }
+
+        [Required]
+        [Display(Name = "City")]
+        public string WeddingCity { get; set; }
+        
+        [Required]
+        [Display(Name = "State")]
+        public string WeddingState { get; set; }
+        
+        [Required]
+        [Display(Name = "Zipcode")]
+        public string WeddingZipcode { get; set; }
+
         public int UserId {get; set;}
 
         public DateTime Created_At { get; set; } = DateTime.Now;
